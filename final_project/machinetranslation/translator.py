2a9b1e2cc43c4759416da0610d53a2bc1ae0c0df
rohitkumarbisht <rk790000@gmail.com>
2023-07-04 12:07:48 -0400

    changes done

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -4,12 +4,12 @@ def english_to_french(english_text):
     '''
     This function translates English text to French
     '''
-    french_text = MyMemoryTranslator(source='en', target='fr').translate(english_text)
+    french_text = MyMemoryTranslator(source='en-US', target='fr-FR').translate(english_text)
     return french_text
 
 def french_to_english(french_text):
     '''
     This function translates French text to English
     '''
-    english_text = MyMemoryTranslator(source='fr', target='en').translate(french_text)
+    english_text = MyMemoryTranslator(source='fr-FR', target='en-US').translate(french_text)
     return english_text
